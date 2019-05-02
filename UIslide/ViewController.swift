71d7c1a371a1688e986a636fd3cc17caa7ba1a1b
D7703_23 <d7703_23@d7703-23ui-imac.local>
2019-05-02 12:19:52 +0900

    Initial Commit

---
+++ UIslide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIslide
+//
+//  Created by D7703_23 on 2019. 5. 2..
+//  Copyright © 2019년 D7703_23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
